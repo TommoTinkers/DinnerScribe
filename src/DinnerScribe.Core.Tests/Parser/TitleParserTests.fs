0775aaac447629a518a86dba12deaa7963c4bd68
Andrew Thompson <mnemonix.lstudios@gmail.com>
2022-05-17 18:18:04 +0100

    Added unhappy path unit test for title parser

--- src/DinnerScribe.Core.Tests/Parser/TitleParserTests.fs
+++ src/DinnerScribe.Core.Tests/Parser/TitleParserTests.fs
@@ -13,8 +13,22 @@ type TitleParserTests () =
     [<TestCase("Title: Beef & Broccoli\r\n More Content Here", "Beef & Broccoli")>]
     [<TestCase("Title: Beef & Broccoli\n More Content Here  Too ", "Beef & Broccoli")>]
     [<TestCase("Title: Mashed Potatoes", "Mashed Potatoes")>]
+    [<TestCase("title: Mashed Potatoes", "Mashed Potatoes")>]
+    [<TestCase("tItle: Mashed Potatoes", "Mashed Potatoes")>]
     member this.ValidInputReturnsValidTitle input expected =
         let result = TitleParser input
         let expectedResult:Result<Title, string> = Ok {Title = expected}
         
         Assert.AreEqual(expectedResult, result)
+        
+    [<Test>]
+    [<TestCase("Mashed Potatoes")>]
+    [<TestCase("Tytle: Mashed Potatoes")>]
+    [<TestCase("Title Mashed Potatoes")>]
+    member this.InvalidInputReturnsError input =
+        let result = TitleParser input
+        match result with
+        | Error _ -> Assert.Pass ()
+        | _ -> Assert.Fail ()
+         
+        
--- src/DinnerScribe.Core/Parser/TitleParser.fs
+++ src/DinnerScribe.Core/Parser/TitleParser.fs
@@ -4,7 +4,7 @@ open DinnerScribe.Core.RecipeModel.Types
 open FParsec
 
 let TitleParser input =
-    let parser = pstring "Title:" >>. spaces >>. restOfLine true |>> fun x -> {Title = x}
+    let parser = pstringCI "Title:" >>. spaces >>. restOfLine true |>> fun x -> {Title = x}
     let result = run parser input
     match result with
     | Success(title, _, _) -> FSharp.Core.Ok title
